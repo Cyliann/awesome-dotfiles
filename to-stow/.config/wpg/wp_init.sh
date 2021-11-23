ce098dae2efaa9fc76b80d1832bae1723c4ae2af
Cyliann <maks.cych@wp.pl>
2021-11-23 22:19:56 +0100

    Usual small updates; Added colorls

--- to-stow/.config/wpg/wp_init.sh
+++ to-stow/.config/wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs '/home/cylian/Pictures/Wallpapers/alena_aenami_dreamer.jpg' '/home/cylian/Pictures/Wallpapers/alena_aenami_dreamer.jpg'
+wpg -rs '/home/cylian/Pictures/Wallpapers/alena_aenami_7p-m.jpg' '/home/cylian/Pictures/Wallpapers/alena_aenami_7p-m.jpg'
